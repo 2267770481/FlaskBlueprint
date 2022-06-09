15466ef9537ffff85ce4b080276acf9eb983fa24
Along <48823256+2267770481@users.noreply.github.com>
2022-06-09 17:59:38 +0800

    Update app.py

--- FlaskBlueprint/app.py
+++ FlaskBlueprint/app.py
@@ -8,12 +8,10 @@
 # def hello_world():
 #     return 'Hello World!'
 
-from flask_script import Manager
 from app import init_app
 
 
 app = init_app()
-mgr = Manager(app)
 
 if __name__ == '__main__':
-    mgr.run()
+    app.run()
